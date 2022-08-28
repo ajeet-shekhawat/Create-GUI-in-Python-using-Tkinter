1ac80ba8b6cb3b16a00aaaeeee3de1bc686e18af
Avani <avanipatel.sculptsoft@gmail.com>
2022-08-28 22:52:31 +0530

    updated

--- test.py
+++ test.py
@@ -1,8 +1,60 @@
-from tkinter import *
+# import tkinter as tk
 
+# class window2:
+#     def __init__(self, master1):
+#         self.panel2 = tk.Frame(master1)
+#         self.panel2.grid()
+#         self.button2 = tk.Button(self.panel2, text = "Quit", command = self.panel2.quit)
+#         self.button2.grid()
+#         vcmd = (master1.register(self.validate),
+#                 '%d', '%i', '%P', '%s', '%S', '%v', '%V', '%W')
+#         self.text1 = tk.Entry(self.panel2, validate = 'key', validatecommand = vcmd)
+#         self.text1.grid()
+#         self.text1.focus()
+
+#     def validate(self, action, index, value_if_allowed,
+#                        prior_value, text, validation_type, trigger_type, widget_name):
+#         if value_if_allowed:
+#             try:
+#                 float(value_if_allowed)
+#                 return True
+#             except ValueError:
+#                 return False
+#         else:
+#             return False
+
+# root1 = tk.Tk()
+# window2(root1)
+# root1.mainloop()
+from tkinter import *
 root = Tk()
 
-button = CustomButton(root, 100, 25, 'red')
-button.pack()
+# function to validate mark entry
+def validate(action, index, value_if_allowed,
+                       prior_value, text, validation_type, trigger_type, widget_name):
+        if value_if_allowed:
+            try:
+                float(value_if_allowed)
+                return True
+            except ValueError:
+                return False
+        else:
+            return False
 
+# def validate(val):
+#     try:
+#         float(val)
+#         return True
+#     except:
+#         return False
+
+# vcmd = (root.register(validate),'%P')
+
+vcmd = (root.register(validate),
+                '%d', '%i', '%P', '%s', '%S', '%v', '%V', '%W')
+
+#text box to enter marks
+e=Entry(root,validate="key", validatecommand=vcmd)
+e.pack()
+e.focus()
 root.mainloop()
